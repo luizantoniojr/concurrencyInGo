ab347166dc05d0c1f4f1073500916c077eafe61d
silvajuniorluizantonio <silvajuniorluizantonio@outlook.com>
2023-11-17 12:22:09 -0300

    Chapter 1: Race Conditions

--- src/chapter1/raceConditions/main.go
+++ src/chapter1/raceConditions/main.go
@@ -15,4 +15,9 @@ func main() {
 	if data == 0 {
 		fmt.Printf("The value is %v.\n", data)
 	}
+
+	//Possible outcomes:
+	//1. Nothing is printed. In this case, line 11 is executed before line 16.
+	//2. The value is 0. In this case, lines 15 and 16 is executed before line 11.
+	//3. The value is 1. In this case, line 15 is executed before line 11, but the goroutine is executed before line 16.
 }
